2785a16ab1056498dd3d90670a2666c329022a37
HikaruCHUJO <hikaru3230811@gmail.com>
2024-06-05 22:22:04 +0200

    script.js

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,3 @@
 function scrollToPage(pageId) {
     document.getElementById(pageId).scrollIntoView({ behavior: 'smooth' });
 }
-function toggleRed() {
-    var button = document.getElementById("myButton");
-    button.classList.toggle("red");
-}
